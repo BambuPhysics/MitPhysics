349c59437ea3563bdfbdb24ad25f39c3c78ce239
dabercro <daniel.r.abercrombie@gmail.com>
2015-07-28 14:32:21 -0400

    Last second commit before I rewrite this mod.

--- Mods/interface/RemoveMuonsMod.h
+++ Mods/interface/RemoveMuonsMod.h
@@ -12,6 +12,7 @@
 #define MITPHYSICS_MODS_REMOVEMUONSMOD_H
 
 #include "MitAna/TreeMod/interface/BaseMod.h" 
+#include "MitAna/DataTree/interface/MuonFwd.h"
 #include "MitAna/DataTree/interface/PFCandidateCol.h"
 #include "MitAna/DataCont/interface/Types.h"
 #include "MitAna/DataTree/interface/TriggerObjectFwd.h"
@@ -25,18 +26,21 @@ namespace mithep
                    const char *title = "Remove Muon Module" );
    ~RemoveMuonsMod();
 
-    const char    *GetTriggerMatchName()     const   { return fTriggerMatchName;   }
-    const char    *GetTriggerObjectsName()   const   { return fTriggerObjectsName; }
-    const char    *GetInputName()            const   { return fInputName;          }
-    const char    *GetOutputName()           const   { return fOutputName;         }
+    const char    *GetTriggerMatchName()     const   { return fTriggerMatchName;      }
+    const char    *GetTriggerObjectsName()   const   { return fTriggerObjectsName;    }
+    const char    *GetPFCandidateName()      const   { return fPFCandidateName;       }
+    const char    *GetMuonName()             const   { return fMuonName;              }
+    const char    *GetOutputName()           const   { return fOutputName;            }
 
-    void SetTriggerMatchName( const char *name )     { fTriggerMatchName = name;   }
-    void SetTriggerObjectsName( const char *name )   { fTriggerObjectsName = name; }
-    void SetInputName( const char *name )            { fInputName = name;          }
-    void SetOutputName( const char *name )           { fOutputName = name;         }
+    void SetTriggerMatchName( const char *name )     { fTriggerMatchName = name;      }
+    void SetTriggerObjectsName( const char *name )   { fTriggerObjectsName = name;    }
+    void SetPFCandidateName( const char *name )      { fPFCandidateName = name;       }
+    void SetOutputName( const char *name )           { fOutputName = name;            }
 
-    void SetInputFromBranch( Bool_t from )           { fInputFromBranch = from;    }
-    void SetDeltaR( Double_t dr )                    { fDeltaR = dr;               }
+    void SetPFCandidateFromBranch( Bool_t from )     { fPFCandidateFromBranch = from; }
+    void SetMuonFromBranch( Bool_t from )            { fMuonFromBranch = from;        }
+    void SetDeltaR( Double_t dr )                    { fDeltaR = dr;                  }
+    void GettingTriggerMatch( Bool_t get )           { fGettingTriggerMatch = get;    }
 
   protected:
     void    SlaveBegin();
@@ -45,14 +49,18 @@ namespace mithep
 
     const TriggerObjectCol *fTriggerObjects;
     const PFCandidateCol   *fPFCandidates;
+    const MuonOArr         *fMuons;
 
     TString fTriggerMatchName;
     TString fTriggerObjectsName;
-    TString fInputName;
+    TString fPFCandidateName;
+    TString fMuonName;
     TString fOutputName;
 
-    Bool_t  fInputFromBranch;
-    Bool_t  fDeltaR;
+    Bool_t   fPFCandidateFromBranch;
+    Bool_t   fMuonFromBranch;
+    Double_t fDeltaR;
+    Bool_t   fGettingTriggerMatch;
 
     ClassDef(RemoveMuonsMod, 1)
   };
--- Mods/src/RemoveMuonsMod.cc
+++ Mods/src/RemoveMuonsMod.cc
@@ -2,6 +2,7 @@
 
 #include "MitPhysics/Mods/interface/RemoveMuonsMod.h"
 #include "MitPhysics/Init/interface/ModNames.h"
+#include "MitCommon/MathTools/interface/MathUtils.h"
 #include "MitPhysics/Utils/interface/ParticleMapper.h"
 #include "MitAna/DataTree/interface/TriggerObjectCol.h"
 
@@ -16,10 +17,11 @@ RemoveMuonsMod::RemoveMuonsMod(const char *name, const char *title) :
   fPFCandidates(0),
   fTriggerMatchName(""),
   fTriggerObjectsName(Names::gkHltObjBrn),
-  fInputName(Names::gkPFCandidatesBrn),
+  fPFCandidateName(Names::gkPFCandidatesBrn),
   fOutputName("PFCandidateMinusMuons"),
-  fInputFromBranch(true),
-  fDeltaR(0.1)
+  fPFCandidateFromBranch(true),
+  fDeltaR(0.1),
+  fGettingTriggerMatch(false)
 { }
 
 //--------------------------------------------------------------------------------------------------
@@ -29,8 +31,10 @@ RemoveMuonsMod::~RemoveMuonsMod() {}
 void RemoveMuonsMod::SlaveBegin()
 {
   // ===== load branches ====
-  if(fInputFromBranch)
-    ReqBranch(fInputName, fPFCandidates);
+  if (fPFCandidateFromBranch)
+    ReqBranch(fPFCandidateName, fPFCandidates);
+  if (fMuonFromBranch)
+    ReqBranch(fMuonName, fMuons);
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -39,16 +43,51 @@ void RemoveMuonsMod::SlaveTerminate() {}
 //--------------------------------------------------------------------------------------------------
 void RemoveMuonsMod::Process()
 {
+
   // Process entries of the tree. 
-  if (fInputFromBranch) 
-    LoadBranch(fInputName);
+  if (fPFCandidateFromBranch) 
+    LoadBranch(fPFCandidateName);
   else
-    LoadEventObject(fInputName, fPFCandidates);
+    LoadEventObject(fPFCandidateName, fPFCandidates);
+
+  if (fMuonFromBranch) 
+    LoadBranch(fMuonName);
+  else
+    LoadEventObject(fMuonName, fMuons);
+
+  ParticleMapper *mapper = new ParticleMapper();
+  mapper->Initialize(*fPFCandidates,fDeltaR,fDeltaR);
 
   PFCandidateArr *PFParticlesMinusMuons = new PFCandidateArr(16);
   PFParticlesMinusMuons->SetName(fOutputName);
 
   LoadEventObject(fTriggerObjectsName, fTriggerObjects);
 
+  std::vector<Int_t> removeThese;
 
+  for (UInt_t i0 = 0; i0 < fTriggerObjects->GetEntries(); i0++) {
+    if (fGettingTriggerMatch) {
+      std::cout << fTriggerObjects->At(i0)->ModuleName() << std::endl;
+      fGettingTriggerMatch = false;
+    }
+    if (fTriggerMatchName == fTriggerObjects->At(i0)->ModuleName()) {
+      std::vector<Int_t> compareThese = mapper->GetNearEtaPhi(fTriggerObjects->At(i0)->Eta(),
+                                                              fTriggerObjects->At(i0)->Phi());
+      for (UInt_t i1 = 0; i1 < compareThese.size(); i1++){
+        if (fPFCandidates->At(compareThese[i1])->PFType() == 3 &&
+            MathUtils::DeltaR(fPFCandidates->At(compareThese[i1]),fTriggerObjects->At(i0)) < fDeltaR) {
+          Bool_t found = false;
+          for (UInt_t i2 = 0; i2 < removeThese.size(); i2++) {
+            if (compareThese[i1] == removeThese[i2]) {
+              found = true;
+              break;
+            }
+          }
+          if (!found)
+            removeThese.push_back(compareThese[i1]);
+        }
+      }
+    }
+  }
+  std::cout << removeThese.size() << std::endl;
 }
